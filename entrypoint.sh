52880cff7983ee1755caa3778ae73949ed358f75
Chris <hel_mort@hotmail.com>
2020-01-06 18:01:18 +0000

    fix(docker): automated changing .env vars to match docker-compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,13 @@ if test -f ".env"; then
 else
     echo "generating .env from .env.example"
     cp .env.example .env
+
+    # gets docker root pw from docker-compose.yml
+    ROOT_PW=$(sed -nE 's/.*MYSQL_ROOT_PASSWORD: (.*)$/\1/p' docker-compose.yml)
+    # changes existing DB_HOST in .env to match the docker-compose database service
+    sed -r -i 's/DB_HOST=.*/DB_HOST=database/g' .env
+    # changes DB_PASSWORD in .env to match root password from docker-compose
+    sed -r -i 's/DB_PASSWORD=.*/DB_PASSWORD='"$ROOT_PW"'/g' .env    
 fi
 
 # Run composer install
