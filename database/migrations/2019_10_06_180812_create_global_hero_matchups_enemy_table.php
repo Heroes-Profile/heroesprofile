85ef72124621e4f4c5629c2dc767d6a8b9ef218f
Zemill <33404722+Zemill@users.noreply.github.com>
2020-01-26 18:27:09 -0500

    db: Added region to globals tables

--- database/migrations/2019_10_06_180812_create_global_hero_matchups_enemy_table.php
+++ database/migrations/2019_10_06_180812_create_global_hero_matchups_enemy_table.php
@@ -25,11 +25,12 @@ class CreateGlobalHeroMatchupsEnemyTable extends Migration
           $table->tinyInteger('hero');
           $table->tinyInteger('enemy');
           $table->tinyInteger('mirror');
+          $table->integer('region');
           $table->tinyInteger('win_loss');
           $table->integer('games_played');
-          
-          $table->primary(['game_version', 'game_type', 'league_tier', 'hero_league_tier', 'role_league_tier', 'game_map', 'hero_level', 'hero', 'enemy', 'mirror', 'win_loss'], 'Primary_Index');
-          $table->index(['game_version', 'game_type', 'hero', 'league_tier', 'hero_league_tier', 'role_league_tier', 'game_map', 'hero_level', 'enemy', 'mirror', 'win_loss', 'games_played'], 'Base_Index');
+
+          $table->primary(['game_version', 'game_type', 'league_tier', 'hero_league_tier', 'role_league_tier', 'game_map', 'hero_level', 'hero', 'enemy', 'mirror', 'region', 'win_loss'], 'Primary_Index');
+          $table->index(['game_version', 'game_type', 'hero', 'league_tier', 'hero_league_tier', 'role_league_tier', 'game_map', 'hero_level', 'enemy', 'mirror', 'region', 'win_loss', 'games_played'], 'Base_Index');
         });
     }
 
