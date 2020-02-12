91f06b596fce60f778cc9e55ee8567fe755655e5
Chris <cmillhou@gmail.com>
2020-02-12 11:28:03 -0500

    fix:  Updated Global Hero Stats backend pull structure

--- app/Data/GlobalHeroStatData.php
+++ app/Data/GlobalHeroStatData.php
@@ -1,11 +1,11 @@
 <?php
 namespace App\Data;
 use Illuminate\Support\Facades\DB;
+use Session;
 
 /*
 use Illuminate\Support\Facades\DB;
 use Cache;
-use Session;
 use App\Battletag;
 use App\LeagueBreakdown;
 use App\LeagueTier;
@@ -14,7 +14,9 @@ use DateTime;
 
 class GlobalHeroStatData
 {
-  private $game_version;
+  private $timeframe;
+  private $game_versions;
+
   private $game_type;
   private $player_league_tier;
   private $hero_league_tier;
@@ -24,20 +26,20 @@ class GlobalHeroStatData
   private $mirror;
   private $region;
 
-  public static function instance($game_version, $game_type, $player_league_tier,
+  public static function instance($timeframe, $game_type, $player_league_tier,
                                $hero_league_tier, $role_league_tier, $game_map, $hero_level,
                                $mirror, $region)
   {
-    return new GlobalHeroStatData($game_version, $game_type, $player_league_tier,
+    return new GlobalHeroStatData($timeframe, $game_type, $player_league_tier,
                                  $hero_league_tier, $role_league_tier, $game_map, $hero_level,
                                  $mirror, $region);
   }
 
 
-  public function __construct($game_version, $game_type, $player_league_tier,
+  public function __construct($timeframe, $game_type, $player_league_tier,
                                $hero_league_tier, $role_league_tier, $game_map, $hero_level,
                                $mirror, $region) {
-    $this->game_version = $game_version;
+    $this->timeframe = $timeframe;
     $this->game_type = $game_type;
     $this->player_league_tier = $player_league_tier;
     $this->hero_league_tier = $hero_league_tier;
@@ -49,7 +51,7 @@ class GlobalHeroStatData
   }
 
   private function getHeroWinLosses(){
-    $sub_query = \App\GlobalHeroStats::Filters($this->game_version, $this->game_type, $this->player_league_tier,
+    $sub_query = \App\GlobalHeroStats::Filters($this->game_versions, $this->game_type, $this->player_league_tier,
                                           $this->hero_league_tier, $this->role_league_tier, $this->game_map, $this->hero_level, $this->mirror, $this->region)
                    ->select('hero', 'win_loss', DB::raw('SUM(games_played) as games_played'))
                    ->groupBy('hero', 'win_loss');
@@ -65,12 +67,18 @@ class GlobalHeroStatData
       ->groupBy('hero')
       ->get();
 
+      /*
+      print_r($sub_query->toSql());
+      echo "<br>";
+      print_r($sub_query->getBindings());
+      echo "<br>";
+      */
       return $global_hero_data;
   }
 
   private function getHeroBans(){
-    $global_ban_data = \App\GlobalHeroBans::Filters(array('2.49.2.77981'), array('1', '5'), array(),
-                                          array(), array(), array(), array(), array())
+    $global_ban_data = \App\GlobalHeroBans::Filters($this->game_versions, $this->game_type, $this->player_league_tier,
+                                          $this->hero_league_tier, $this->role_league_tier, $this->game_map, $this->hero_level, $this->region)
                       ->select('hero', DB::raw('SUM(bans) as games_banned'))
                       ->groupBy('hero')
                       ->get();
@@ -78,9 +86,70 @@ class GlobalHeroStatData
   }
 
   private function getHeroChange(){
+    if(count($this->timeframe) == 1
+      && count($this->game_type) == 1
+      && $this->game_type[0] != "br"
+      && count($this->game_map) == 0
+      && count($this->player_league_tier) == 0
+      && count($this->hero_league_tier) == 0
+      && count($this->role_league_tier) == 0
+      && count($this->hero_level) == 0
+      && count($this->region) == 0){
+      $major_season_game_version = \App\SeasonGameVersions::where('game_version', '>=', '2.43')
+                                  ->select(DB::raw("DISTINCT(SUBSTRING_INDEX(game_version, '.', 2)) as game_version"))
+                                  ->orderBy('game_version', 'DESC')
+                                  ->get();
+      $major_season_game_version = json_decode(json_encode($major_season_game_version),true);
+      $found = 0;
+      $timeframe = "";
+      if(count($major_season_game_version) > 0){
+        for($i = 0; $i < count($major_season_game_version); $i++){
+          if($found){
+            $timeframe = $major_season_game_version[$i]["game_version"];
+            break;
+          }
+          if($major_season_game_version[$i]["game_version"] == $this->timeframe[0]){
+            $found = 1;
+          }
+        }
+      }
+
+      if(!$found){
+        foreach (Session::get("all_minor_patch") as $key => $value){
+          if($found){
+            $timeframe = $value;
+            break;
+          }
+          if($value == $this->timeframe[0]){
+            $found = 1;
+          }
+        }
+      }
+
+
+      $change_data = \App\GlobalHeroChange::where('game_version', $timeframe)
+                        ->where('game_type', $this->game_type[0])
+                        ->select('hero', 'win_rate')
+                        ->get();
+      return $change_data;
+    }
   }
 
   private function combineData(){
+    $game_version_counter = 0;
+    for($i = 0; $i < count($this->timeframe); $i++){
+      if(in_array($this->timeframe[$i], Session::get("all_major_patch"))){
+        for($j = 0; $j < count(Session::get("major_to_minor_patch_mapping")[$this->timeframe[$i]]); $j++){
+          $this->game_versions[$game_version_counter] = Session::get("major_to_minor_patch_mapping")[$this->timeframe[$i]][$j];
+          $game_version_counter++;
+        }
+
+      }else{
+        $this->game_versions[$game_version_counter] = $this->timeframe[$i];
+        $game_version_counter++;
+      }
+    }
+
     $global_hero_data = $this->getHeroWinLosses();
     $global_ban_data = $this->getHeroBans();
     $global_change_data = $this->getHeroChange();
@@ -91,8 +160,8 @@ class GlobalHeroStatData
     for($i = 0; $i < count($global_hero_data); $i++){
       $global_hero_data[$i]->games_played = $global_hero_data[$i]->wins + $global_hero_data[$i]->losses;
       if($global_hero_data[$i]->games_played){
-        $global_hero_data[$i]->win_rate = ($global_hero_data[$i]->wins / ($global_hero_data[$i]->wins + $global_hero_data[$i]->losses)) * 100;
-        $global_hero_data[$i]->pick_rate = ($global_hero_data[$i]->games_played / $total_games) * 100;
+        $global_hero_data[$i]->win_rate = $global_hero_data[$i]->wins / ($global_hero_data[$i]->wins + $global_hero_data[$i]->losses);
+        $global_hero_data[$i]->pick_rate = $global_hero_data[$i]->games_played / $total_games;
       }else{
         $global_hero_data[$i]->win_rate = 0;
         $global_hero_data[$i]->pick_rate = 0;
@@ -105,14 +174,22 @@ class GlobalHeroStatData
         }
       }
 
+      if(count($global_change_data) > 0){
+        foreach ($global_change_data as $change_data) {
+          if($change_data->hero == $global_hero_data[$i]->hero){
+            $global_hero_data[$i]->change = ($global_hero_data[$i]->win_rate * 100)  - $change_data->win_rate;
+            break;
+          }
+
+        }
+      }
+
       if($global_hero_data[$i]->games_banned > 0){
         $global_hero_data[$i]->popularity = ($global_hero_data[$i]->games_played / $total_games) * 100;
       }else{
         $global_hero_data[$i]->popularity = (($global_hero_data[$i]->games_banned + $global_hero_data[$i]->games_played) / $total_games) * 100;
       }
-
-
-      //$return_data[$i]["influence"] = round(($return_data[$i]["win_rate_influence"] - .5) * ($return_data[$i]["adjusted_pick_rate"] * 10000));
+      $global_hero_data[$i]->influence = round(($global_hero_data[$i]->win_rate - .5) * ($global_hero_data[$i]->pick_rate * 10000));
     }
 
     return $global_hero_data;
--- config/database.php
+++ config/database.php
@@ -67,6 +67,30 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+        'mysql_cache' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'read' => [
+                'host' => env('READ_DB_HOST', '127.0.0.1'),
+            ],
+            'write' => [
+                'host' => env('WRITE_DB_HOST', '127.0.0.1'),
+            ],
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE_CACHE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
