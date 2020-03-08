de4eeed67b2223aa8d7534b4bbe76314661c72bc
Rowan Christmas <rowanxmas@gmail.com>
2020-03-07 19:58:25 -0800

    ran npm upgrade to get latest versions

---
+++ resources/js/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import GlobalLeaderboard from '@/components/GlobalLeaderboard.vue';
+import HeroSummary from '@/components/Profile/HeroSummary.vue';
+import OptOut from '@/components/OptOut/OptOut.vue';
+
+let routes = [      
+    {
+        path: '/', component: GlobalLeaderboard 
+    },
+    {
+        path: '/profile', component: HeroSummary 
+    },
+    {
+        path: '/optout', component: OptOut 
+    },
+
+]
+
+export default new VueRouter({
+    routes: routes,
+    mode: 'history', //removes # (hashtag) from url
+    base: '/',
+    fallback: true
+})
