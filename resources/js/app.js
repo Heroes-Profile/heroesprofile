26d6d71d3f30335d76f5ede25ad0d18761d9a944
Rowan Christmas <rowanxmas@gmail.com>
2020-03-07 20:02:36 -0800

    re-configure Vue init into modules, change from global component imports to per-component imports

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,101 +1,30 @@
+require('./bootstrap')
+
 import Vue from 'vue';
-import Vuex from 'vuex';
-import VueRouter from 'vue-router';
-import DataTable from './components/DataTable.vue';
-import SearchForm from './components/SearchForm.vue';
-import BoostrapVue from 'bootstrap-vue';
-import ThemeSwitcher from './components/ThemeSwitcher.vue';
+import store from './store'
+import router from './router'
+
 import VueCookie from 'vue-cookie';
-import ImagePopup from './components/ImagePopup.vue';
-import Login from './components/Login.vue';
-import Multiselect from 'vue-multiselect';
-import HeroTalentData from './components/HeroTalentData.vue';
-import StatBox from './components/StatBox.vue';
-
-//Profile Vue Components
-import TabSwitcher from './components/Profile/TabSwitcher.vue';
-import HeroSummary from './components/Profile/HeroSummary.vue';
-
-import { routes } from './routes';
-import vClickOutside from 'v-click-outside';
-require('./bootstrap');
-
-
-window.Vue = require('vue');
-
-Vue.use(VueRouter);
-Vue.use(BoostrapVue);
 Vue.use(VueCookie);
-Vue.use(Vuex);
-Vue.use(vClickOutside);
-Vue.component('theme-switcher', require('./components/ThemeSwitcher.vue').default);
-Vue.component('data-table', require('./components/DataTable.vue').default);
-Vue.component('search-form', require('./components/SearchForm.vue').default);
-Vue.component('image-popup', require('./components/ImagePopup.vue').default);
-Vue.component('login', require('./components/Login.vue').default);
-Vue.component('multiselect', Multiselect);
-Vue.component('hero-talent-data', HeroTalentData);
-let handleOutsideClick
 
-const store = new Vuex.Store({
-  state: {
-    formFields: {},
-    formData: [],
-    timeframe: [],
-    currentAjaxURL: '',
-    loading: true
-  },
-  mutations: {
-    updateFormFields (state, fields){
-      state.loading = true
-      state.formFields = fields
-    },
-    formData (state, fields){
-      state.formData = fields
-      state.loading = false
-    },
-    updateAjaxURL (state, fields){
-      state.currentAjaxURL = fields
-    }
-  },
-  actions:{
-    updateFormData ({ commit, state }, fields){
-      commit('updateFormFields',  fields);
-      /*
-      var timeframe = [];
-      if(fields.timeframe != undefined){
-          for (var i = 0; i < fields.timeframe.length; i++) {
-            timeframe.push(fields.timeframe[i].value.replace(/['"]+/g, ''));
-        }
-      }
-      timeframe = timeframe.join(',');
-      */
-      axios.post(state.currentAjaxURL, {
-        headers: {
-          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        },
-        params: {
-          'data' : fields
-        }
-      }).then(response => {
-        console.log(response.data);
-           commit('formData', response.data);
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-         });
-    }
-  }
-})
+import BoostrapVue from 'bootstrap-vue'
+Vue.use(BoostrapVue);
 
-//Profile Vue Components
-Vue.component('profile-tab-switcher', TabSwitcher);
-Vue.component('hero-summary', HeroSummary);
-Vue.component('stat-box', StatBox);
+// TODO: What is this?
+// import vClickOutside from 'v-click-outside';
+// Vue.use(vClickOutside);
 
-const router = new VueRouter({
-    mode: 'history',
-    routes
-});
+import HeroesProfileApp from './HeroesProfileApp'
 const app = new Vue({
-  router,
-  store
-}).$mount('#app')
+	el: '#app',
+	router,
+	store,
+	template: `<heroes-profile-app></heroes-profile-app>`,
+	components: {HeroesProfileApp},
+	mounted () {
+		console.log('app.js mounted')
+	}
+})
