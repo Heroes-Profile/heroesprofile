802f9f66e9c4d4442749b86e143d15f7a6ffc45c
Rowan Christmas <rowanxmas@gmail.com>
2020-03-09 21:23:32 -0700

    remove extra quote from the meta tag

--- resources/views/scripts/header.blade.php
+++ resources/views/scripts/header.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<meta id="myViewport" name="viewport" content="width=device-width, initial-scale=1,">
+<meta id="myViewport" name="viewport" content="width=device-width, initial-scale=1">
 
 <title>Heroes Profile @yield('title')</title>
 <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
