ddd4b6435d5f8097f5d1d361cf8a3f04989cab09
Rowan Christmas <rowanxmas@gmail.com>
2020-03-09 21:26:00 -0700

    correctly have the SearchBar on top of the DataTable

--- resources/js/components/GlobalLeaderboard.vue
+++ resources/js/components/GlobalLeaderboard.vue
@@ -1,18 +1,22 @@
 <template>
-    <div>
-        <data-table></data-table>
-		<search-form></search-form>
+  <div>
+    <div class="intro">
+      <h2>Global Win Rates</h2>
+     <div><p>Hero win rates based on differing increments, stat types, game type, or league tier.</p></div>
     </div>
+    <search-form></search-form>
+    <data-table></data-table>
+  </div>
 </template>
 
 <script>
-import DataTable from '@/components/DataTable.vue';
-import SearchForm from '@/components/SearchForm.vue';
+import DataTable from "@/components/DataTable.vue";
+import SearchForm from "@/components/SearchForm.vue";
 
 export default {
-    components: {
-		DataTable,
-		SearchForm
-	}
-}
+  components: {
+    DataTable,
+    SearchForm
+  }
+};
 </script>
