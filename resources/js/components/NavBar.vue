eb3ef0d80b8252d0fff900dcfe6f2f6039318fcf
Rowan Christmas <rowanxmas@gmail.com>
2020-03-09 21:26:20 -0700

    bring back the NavBar design from the Alpha

--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -1,43 +1,72 @@
 <template>
-  <div class="hp-tabs">
-    <router-link 
-      v-for="tab in tabs" 
-      :key="tab.route" 
-      :to="tab.route" 
-      tag="button"
-      exact
-      active-class="btn-primary">
-      {{tab.title}}
-    </router-link>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="/">
+      Heroes
+      <img
+        src="/images/logo/heroesprofilelogo.png"
+        width="34"
+        height="30"
+        class="d-inline-block align-top"
+        alt
+      />
+      Profile
+    </a>
+    <!-- <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="true"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>-->
 
-    </div>
+    <theme-switcher></theme-switcher>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+          <router-link
+            v-for="tab in tabs"
+            :key="tab.route"
+            :to="tab.route"
+            tag="li"
+            class="m2-3 px-2"
+            exact
+            active-class="btn-primary"
+          >{{tab.title}}</router-link>
+      </ul>
+      </div>
+  </nav>
 </template>
 
 <style lang="sass" scoped>
-
-
-
 </style>
 
 <script>
-  export default {
-    data: function () {
-      return {
-        tabs: [
-          {
-            title: 'Global Leaderboard',
-            route: "/"
-          },
-          {
-            title: "Profile",
-            route: "/profile"
-          },
-           {
-            title: "OptOut",
-            route: "/optout"
-          }
-        ]
-      }
+import ThemeSwitcher from "@/components/ThemeSwitcher.vue";
+
+export default {
+  components: {
+    ThemeSwitcher
+  },
+  data: function() {
+    return {
+      tabs: [
+        {
+          title: "Global Win Rates",
+          route: "/"
+        },
+        {
+          title: "Profile",
+          route: "/profile"
+        },
+        {
+          title: "OptOut",
+          route: "/optout"
+        }
+      ]
+    };
   }
-}
+};
 </script>
