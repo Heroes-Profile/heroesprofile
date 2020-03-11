71ad20276f049de2238ad08ffc6eb5951df0cb0b
Avadriel <tathiebaut@gmail.com>
2020-03-11 19:21:00 -0400

    feat(optOut): IR #85
    Implemented a temporary workaround for this issue.

--- resources/views/optout/failure.blade.php
+++ resources/views/optout/failure.blade.php
@@ -1,1 +1,29 @@
-Fail
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <title>Heroes Profile | Opt Out </title>
+        <meta name="robots" content="noindex">
+<link rel="stylesheet" href="http://127.0.0.1:8000/css/app.css">
+
+    </head>
+    <body>
+
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark"><a href="/" class="navbar-brand">
+      Heroes
+      <img src="/images/logo/heroesprofilelogo.png" width="34" height="30" alt="" class="d-inline-block align-top">
+      Profile
+      </a> <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler"><span class="navbar-toggler-icon"></span></button> <div><div role="group" btn-group-toggle="" class="btn-group"> <div id="navbarSupportedContent" class="collapse navbar-collapse"></div></nav>
+      <div class="intro"><h2>Opt Out - Failure</h2> <div></div></div>
+
+<div class="container" style="padding-top:20px; text-align:center;">
+
+
+<div class="alert alert-danger" role="alert">There was an issue processing your opt out request. Please try again.</div>
+
+
+
+</div>
+
+</body>
+</html>
