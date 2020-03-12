3d6f1fe55c80b0ea1457aa64d13ea65dbe213ffa
Zemill <33404722+Zemill@users.noreply.github.com>
2020-03-11 20:01:48 -0400

    fix(optout): updated wording on required check box text

--- resources/views/optout/optout.blade.php
+++ resources/views/optout/optout.blade.php
@@ -24,13 +24,13 @@
   <p>If you do not wish for your battletag to appear on the site or API, we provide the option for you to opt-out. In order to do this, we require that you authenticate with your Blizzard account. This ensures that you are the player with that Battletag and ensures that the system is not abused by those wishing to remove other players' data.</p>
   <p>Please choose a region below, read the disclaimer and confirm, then click "opt-out". You will be redirected to Blizzard's secure authentication and asked to allow access for your Battletag. This is the only information we use, and we do not store this information anywhere. We do not collect or store any personal information. After you have opted out, you may remove your authorization for this website by going to your Battle.net account under "Security & Privacy" and managing your authorized applications.</p>
   <p>Once you have opted-out, data uploaded from replays in which you played a game will still be used to calculate data across the site, but users will be unable to search using your battletag, and your battletag will not show up in games played</p>
-
+<p>Opting out does not guarantee removal of any data from 3rd party sites or users who retreived data from the HeroesProfile API or website prior to opting out.   Heroes Profile is not responsibile for 3rd party sites or users who use our data or for the removal of data sourced from our tools on 3rd party sites or users.</p>
 
   <form action="optout/login" id="optout">
     <div class="form-check">
   <input class="form-check-input" type="checkbox" value="" id="defaultCheck1" required>
   <label class="form-check-label" for="defaultCheck1">
-    I understand that opting out also removes my data from any other sites or applications using Heroes Profile data, which may make me ineligeble for participation in some Amateur series league play. <p>
+    I understand that opting out prevents other sites and users from accessing any of my battletag's data, either through the Heroes Profile main site, or the API site.  <b>This may make me ineligeble for participation in some Amateur Series league play.</b><p>
 
   </label>
 </div>
