93264bd404eac18c254ecceaf99166a5818eb442
Rowan Christmas <rowanxmas@gmail.com>
2020-03-15 14:27:07 -0700

    feature: add data fetching to the Vuex store
    exposes the currently used fields (rawfields, primary, secondary, timeframe_type) as getters off the returned rawfields object from the /api/heroes GET API

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -10,7 +10,26 @@ export default new Vuex.Store({
 		formData: [],
 		timeframe: [],
 		currentAjaxURL: '',
-		loading: true
+		loading: true,
+		rawfields: {}
+	},
+	getters: {
+		rawfields: (state, getters) => {
+			return state.rawfields.rawfields
+		},
+		gameMaps: (state, getters) => {
+			return state.rawfields.rawfields.game_map
+		},
+		primaryFields: (state, getters) => {
+			return state.rawfields.rawfields.primaryFields
+		},
+		secondaryfields: (state, getters) => {
+			return state.rawfields.rawfields.secondaryfields
+		},
+		timeframe_type: (state, getters) => {
+			return state.rawfields.timeframe_type
+		}
+
 	},
 	mutations: {
 		updateFormFields (state, fields){
@@ -23,9 +42,30 @@ export default new Vuex.Store({
 		},
 		updateAjaxURL (state, fields){
 			state.currentAjaxURL = fields
+		},
+		updateInitialResources (state, payload) {
+			state.rawfields = payload
 		}
 	},
 	actions:{
+		INIT_ALL_RESOURCES (context, payload = null) {
+			axios
+			.get("/api/heroes")
+			.then(response => {
+				console.assert(response.data != null, '/api/heroes returned null response data, unable to initialize')
+				context.commit('updateInitialResources', response.data)
+// \			  this.rawfields = response.data.rawfields
+// 			  this.primaryFields = response.data.primaryFields
+// 			  this.secondaryfields = response.data.secondaryfields
+// 			  this.timeframe_type = response.data.timeframe_type
+			//   this.$nextTick( () => {
+			// 	this.loaded = true
+			//   })
+			})
+			.catch(err => {
+				console.error(err)
+			})
+		},
 		updateFormData ({ commit, state }, fields){
 			commit('updateFormFields',  fields);
 			axios.post(state.currentAjaxURL, {
