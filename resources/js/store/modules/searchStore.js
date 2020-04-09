872406e27aa2c42c99d5bad74262d69aa7c7e722
Rowan Christmas <rowanxmas@gmail.com>
2020-04-09 14:40:55 -0700

    refactor: make the title for the filter button be more dynamic for the title and include counts

--- resources/js/store/modules/searchStore.js
+++ resources/js/store/modules/searchStore.js
@@ -7,14 +7,22 @@ export default {
     },
     getters: {
         titleForFilterType : (state, getters) => (filter) => {
+            let title
+            let count
             if (filter === 'game-type-filter') {
-                return state.game_type_selection && state.game_type_selection.length > 0 ? state.game_type_selection.join(", ") : "Storm League"
+                title = state.game_type_selection && state.game_type_selection.length > 0 ? state.game_type_selection.join(", ") : "Storm League"
+                count = state.game_type_selection.length
             }
 
             if (filter === 'hero-filter') {
-                return state.selectedHeroes && state.selectedHeroes.length > 0 ? state.selectedHeroes.join(", ") : 'All Heroes'
+                title = state.selectedHeroes && state.selectedHeroes.length > 0 ? state.selectedHeroes.join(", ") : 'All Heroes'
+                count = state.selectedHeroes.length
             }
-            },
+            if (title.length > 20) {
+                title = title.slice(0,20)+'…'+`(${count})`
+            }
+            return title
+        },
         selectedGameTypes: (state, getters) => {
 			return state.game_type_selection.length === 0 ? [state.defaultGameType] : state.game_type_selection
         },
