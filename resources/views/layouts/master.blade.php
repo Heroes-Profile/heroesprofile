3cfdb076fdd4d4d2e8d0ceafbf51c6bbdc037b72
Chris <cmillhou@gmail.com>
2020-04-18 17:27:30 -0400

    fix:  Adding layouts file

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+      <title>Global Hero Stats</title>
+      <meta charset="utf-8">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.10.1/bootstrap-table.min.css">
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.10.1/bootstrap-table.min.js"></script>
+  </head>
+  <body>
+
+  </body>
+</html>
