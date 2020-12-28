82538fba94a74cc1d6ab571fc8c200a3f35e5ff3
MGatner <mgatner@icloud.com>
2020-12-28 01:01:26 +0000

    Tweak columns, improve layout

--- resources/views/Gamedata/form.blade.php
+++ resources/views/Gamedata/form.blade.php
@@ -1,19 +1,19 @@
 
-	<form name="gamedata-form" method="GET">
+	<form name="gamedata-form" method="GET" class="m-3">
 		@csrf
 		<div class="row">
-			<div class="col">
-				<select name="patch" class="custom-select">
-					<option value="">Patch...</option>
+			<div class="col-6 col-md">
+				<label for="patch" class="text-light">Patch:</label>
+				<select id="patch" name="patch" class="custom-select">
 					@foreach ($patches as $p)
 					<option {{ $p === $patch ? 'selected' : '' }}>{{ $p }}</option>
 					@endforeach
 				</select>
 			</div>
 
-			<div class="col">
-				<select name="locale" class="custom-select">
-					<option value="">Locale...</option>
+			<div class="col-6 col-md">
+				<label for="locale" class="text-light">Locale:</label>
+				<select id="locale" name="locale" class="custom-select">
 					@foreach ($locales as $country => $l)
 					<option value="{{ $l }}" {{ $l === $locale ? 'selected' : '' }}>{{ ucfirst(strtolower($country)) }}</option>
 					@endforeach
@@ -21,7 +21,9 @@
 			</div>
 
 			<div class="col">
-				<button class="btn btn-primary" type="submit">Submit</button>
+				<label for="submit" class="invisible">Submit</label>
+				<br />
+				<button class="btn btn-primary btn-sm mt-1" type="submit">Submit</button>
 			</div>
 		</div>
 	</form>
--- resources/views/Gamedata/hero.blade.php
+++ resources/views/Gamedata/hero.blade.php
@@ -9,9 +9,11 @@
 			<tr>
 				<th scope="col">Name</th>
 				<th scope="col">Title</th>
+				<th scope="col">Type</th>
 				<th scope="col">Role</th>
 				<th scope="col">Difficulty</th>
 				<th scope="col">Description</th>
+				<th scope="col">Info</th>
 			</tr>
 		</thead>
 		<tbody>
@@ -20,9 +22,11 @@
 			<tr>
 				<td>{{ $hero->string('name') }}</td>
 				<td>{{ $hero->string('title') }}</td>
+				<td>{{ $hero->string('type') }}</td>
 				<td>{{ $hero->string('expandedrole') }}</td>
 				<td>{{ $hero->string('difficulty') }}</td>
 				<td>{{ $hero->string('description') }}</td>
+				<td>{{ $hero->string('infotext') }}</td>
 			</tr>
 			@endforeach
 
