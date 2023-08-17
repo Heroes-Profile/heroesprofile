c3421e4f4a93b5ea33ea3242d12b39353edbdb32
Zemill <33404722+Zemill@users.noreply.github.com>
2023-08-17 09:02:07 -0400

    Scaffold

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
