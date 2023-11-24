c8f6d09733e470e208498f6399d63db5dd8e46ac
Tasha Millhouse <tathiebaut@gmail.com>
2023-11-24 13:35:31 -0500

    completed mobile nav - ready for testing

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -12,8 +12,8 @@
 </div>
 
 <div id="main-menu" class="  main-navigation-wrapper"> <!-- max-md:hidden  - This needs to hide/show on click of the button but only on mobile - either detect mobile with vue, or have a class that is added/taken away that only applies to mobile -->
-    <nav class="bg-gray-dark text-white z-40 relative md:px-4 text-sm ">
-      <button  id="mobile-nav-close" class="hidden bg-blue rounded-lg px-2 py-1 ml-auto">x</button>
+    <nav class="bg-gray-dark text-white z-40 relative md:px-4 text-sm max-md:pt-2">
+      <button  id="mobile-nav-close" class="hidden bg-blue rounded-lg px-2 py-1 ml-auto  mr-2">x</button>
        
         <div class="flex items-center justify-between flex-wrap  max-md:flex-col" >
              
@@ -23,7 +23,7 @@
                 Profile
             </a>
             <div class="flex items-center justify-between flex-wrap  text-sm md:ml-auto max-md:flex-col">
-                <div class="flex items-center md:space-x-5 justify-end flex-wrap md:flex-nowrap md:pr-5 max-md:flex-col">
+                <div class="flex items-center md:space-x-5 md:justify-end flex-wrap md:flex-nowrap md:pr-5 max-md:flex-col max-md:w-full ">
                     
                     <div class="relative group inline-block nav-item">
                         <a class=" cursor-pointer">Global Hero Stats</a>
@@ -105,7 +105,7 @@
                 </div>
 
                 {{-- Distinct white or grey line show the seperation here  --}}
-                <div class="flex space-x-5 bg-lighten p-3 mx-2 max-md:order-1">
+                <div class="flex space-x-5 bg-lighten p-3 mx-2 max-md:order-1 max-md:mt-auto">
                     <custom-button :href="'https://api.heroesprofile.com/Api'" :targetblank="true" :text="'API'" :alt="'API'"  :size="'small'" :color="'teal'"></custom-button>
                     <custom-button :href="'https://api.heroesprofile.com/upload'" :targetblank="true" :text="'Replay Uploader'" :alt="'Replay Uploader'"  :size="'small'" :color="'blue'"></custom-button>
                     <custom-button :href="'https://www.patreon.com/heroesprofile'" :targetblank="true" :text="'Patreon'" :alt="'Patreon'"  :size="'small'" :color="'red'"></custom-button>
@@ -113,15 +113,15 @@
 
 
                 @if($isAuthenticated)
-                    <div class="relative group inline-block  ml-5 ">
-                            <div class="flex items-center cursor-pointer mr-5">
+                    <div class="relative group inline-block  md:ml-5 max-md:my-4 max-md:flex items-center">
+                            <div class="flex items-center cursor-pointer md:mr-5">
                                 <img 
                                 class="card-img-top relative hover:opacity-75 w-12 h-12 rounded-full" 
                                 src="/images/heroes/auto_select.jpg" 
                                 alt="Settings">
                             </div>
-                            <div class="absolute right-0 hidden group-hover:block z-50  ">
-                                <div class="bg-blue  rounded-b-lg rounded-tl-lg text-sm drop-shadow">
+                            <div class="md:absolute right-0 md:hidden md:group-hover:block z-50  ">
+                                <div class="md:bg-blue  rounded-b-lg rounded-tl-lg text-sm drop-shadow max-md:flex">
                                     <a href="/Profile/Settings" class="block px-4 py-2 border-b border-darken hover:bg-lighten">Settings</a>
                                     <a href="/Battlenet/Logout" class="block px-4 py-2 border-b border-darken hover:bg-lighten">Logout</a>
                                 </div>
@@ -133,56 +133,22 @@
 
 
                 @else
-                    <custom-button class="ml-4" :href="'/Authenticate/Battlenet'" :text="'Login'" :alt="'Login'" :size="'small'"></custom-button>
+                    <custom-button class="md:ml-4 max-md:my-4" :href="'/Authenticate/Battlenet'" :text="'Login'" :alt="'Login'" :size="'small'"></custom-button>
                 @endif
-                <div class="md:hidden">
-                    @foreach($altSearchAccounts as $index => $account)
-            @if($account)
-                <div class="relative group inline-block  p-4 mx-4 text-sm  ">
-                    <a class=" cursor-pointer">{{ $account['battletag'] }} ({{ $regions[$account['region']] }})</a>
-                    <div class="nav-dropdown ">
-                        <div class="nav-dropdown-inner-wrapper ">
-                        <a href="/Player/{{ $account['battletag'] }}/{{ $account['blizz_id'] }}/{{ $account['region'] }}" >Profile</a>
-                        <a href="/Player/{{ $account['battletag'] }}/{{ $account['blizz_id'] }}/{{ $account['region'] }}/FriendFoe" >Friends and Foes</a>
-                        <a href="/Player/{{ $account['battletag'] }}/{{ $account['blizz_id'] }}/{{ $account['region'] }}/Hero" >Heroes</a>
-                        <a href="/Player/{{ $account['battletag'] }}/{{ $account['blizz_id'] }}/{{ $account['region'] }}/Role">Roles</a>
-                        <a href="/Player/{{ $account['battletag'] }}/{{ $account['blizz_id'] }}/{{ $account['region'] }}/Map" >Maps</a>
-                        <a href="/Player/{{ $account['battletag'] }}/{{ $account['blizz_id'] }}/{{ $account['region'] }}/Matchups">Matchups</a>
-                        <a href="/Player/{{ $account['battletag'] }}/{{ $account['blizz_id'] }}/{{ $account['region'] }}/Talents" >Talents</a>
-                        <a href="/Player/{{ $account['battletag'] }}/{{ $account['blizz_id'] }}/{{ $account['region'] }}/MMR">MMR Breakdown</a>
-                        <a href="/Player/{{ $account['battletag'] }}/{{ $account['blizz_id'] }}/{{ $account['region'] }}/Match/History" >Match History</a>
-                    </div>
-                    </div>
-                </div>
-                 <div class="relative group inline-block  p-4 mx-4 text-sm  ">
-                    <a class=" cursor-pointer">{{ $account['battletag'] }} ({{ $regions[$account['region']] }})</a>
-                    <div class="nav-dropdown ">
-                        <div class="nav-dropdown-inner-wrapper ">
-                        <a href="/Player/{{ $account['battletag'] }}/{{ $account['blizz_id'] }}/{{ $account['region'] }}" >Profile</a>
-                        <a href="/Player/{{ $account['battletag'] }}/{{ $account['blizz_id'] }}/{{ $account['region'] }}/FriendFoe" >Friends and Foes</a>
-                        <a href="/Player/{{ $account['battletag'] }}/{{ $account['blizz_id'] }}/{{ $account['region'] }}/Hero" >Heroes</a>
-                        <a href="/Player/{{ $account['battletag'] }}/{{ $account['blizz_id'] }}/{{ $account['region'] }}/Role">Roles</a>
-                        <a href="/Player/{{ $account['battletag'] }}/{{ $account['blizz_id'] }}/{{ $account['region'] }}/Map" >Maps</a>
-                        <a href="/Player/{{ $account['battletag'] }}/{{ $account['blizz_id'] }}/{{ $account['region'] }}/Matchups">Matchups</a>
-                        <a href="/Player/{{ $account['battletag'] }}/{{ $account['blizz_id'] }}/{{ $account['region'] }}/Talents" >Talents</a>
-                        <a href="/Player/{{ $account['battletag'] }}/{{ $account['blizz_id'] }}/{{ $account['region'] }}/MMR">MMR Breakdown</a>
-                        <a href="/Player/{{ $account['battletag'] }}/{{ $account['blizz_id'] }}/{{ $account['region'] }}/Match/History" >Match History</a>
-                    </div>
-                    </div>
-                </div>
-            @endif
-        @endforeach
-    </div>
+             
             </div>
         </div>
     </nav>
-    <nav class="flex justify-end mr-8 alt-acct-nav">
+    
+</div>
+<nav class="flex justify-end md:mr-8 alt-acct-nav max-md:mt-4">
         @foreach($altSearchAccounts as $index => $account)
             @if($account)
-                <div class="relative group inline-block  p-4 mx-4 text-sm  ">
-                    <a class=" cursor-pointer">{{ $account['battletag'] }} ({{ $regions[$account['region']] }})</a>
-                    <div class="nav-dropdown absolute  hidden group-hover:block z-50 pt-3 absolute right-0 min-w-[200px]">
-                        <div class="nav-dropdown-inner-wrapper ">
+                <div class="relative group inline-block  md:p-4 mx-4 text-sm  ">
+                    <a data-battletag="{{ $account['battletag'] }}" class="mobile-secondary-nav-open cursor-pointer">{{ $account['battletag'] }} ({{ $regions[$account['region']] }})</a>
+                    <div data-battletag="{{ $account['battletag'] }}" class="nav-dropdown absolute  hidden md:group-hover:block z-50 md:pt-3 absolute md:right-0 md:min-w-[200px] max-md:top-0 max-md:fixed max-md:w-full nav-dropdown-secondary-nav max-md:bg-gray-dark max-md:h-full">
+                        <div class="nav-dropdown-inner-wrapper rounded-none">
+                            <div  class="mobile-secondary-nav-name flex justify-between md:hidden  p-3 bg-teal">{{ $account['battletag'] }} ({{ $regions[$account['region']] }}) <button class="close-secondary-nav">x</button></div>
                         <a href="/Player/{{ $account['battletag'] }}/{{ $account['blizz_id'] }}/{{ $account['region'] }}" >Profile</a>
                         <a href="/Player/{{ $account['battletag'] }}/{{ $account['blizz_id'] }}/{{ $account['region'] }}/FriendFoe" >Friends and Foes</a>
                         <a href="/Player/{{ $account['battletag'] }}/{{ $account['blizz_id'] }}/{{ $account['region'] }}/Hero" >Heroes</a>
@@ -198,6 +164,5 @@
             @endif
         @endforeach
     </nav>
-</div>
 <mobile-nav-hack></mobile-nav-hack>
 
