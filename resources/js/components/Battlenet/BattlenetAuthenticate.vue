c1663db19d8b63fea5df2b6c3e96feca107b12bd
Zemill <33404722+Zemill@users.noreply.github.com>
2024-01-03 12:39:28 -0500

    Adds in login error catch/handling

--- resources/js/components/Battlenet/BattlenetAuthenticate.vue
+++ resources/js/components/Battlenet/BattlenetAuthenticate.vue
@@ -5,10 +5,10 @@
     <p>If you want to access your profile, you need to log in through Battle.net.</p>
     
     <div class="flex items-end my-20 justify-center">
-    <single-select-filter :values="filters.regions" :text="'Region'" v-model="selectedRegion" @input-changed="handleInputChange"></single-select-filter>
+      <single-select-filter :values="filters.regions" :text="'Region'" v-model="selectedRegion" @input-changed="handleInputChange"></single-select-filter>
 
-    <custom-button class="m-2" :href="'/redirect/authenticate/battlenet'" :text="'Login with Battle.net'" :alt="'Login with Battle.net'" :size="'medium'" :color="'blue'" :disabled="!selectedRegion"></custom-button>
-  </div>
+      <custom-button class="m-2" :href="'/redirect/authenticate/battlenet'" :text="'Login with Battle.net'" :alt="'Login with Battle.net'" :size="'medium'" :color="'blue'" :disabled="!selectedRegion"></custom-button>
+    </div>
 
    <p class="text-sm"> Heroes Profile uses Blizzard's Battle.net OAuth for authentication.  Battle.net OAuth is an authentication and authorization framework used by Blizzard Entertainment for its online gaming platform, Battle.net. It allows users to securely log in to Battle.net and grant third-party applications limited access to their Battle.net accounts without sharing their account credentials. </p>
   </div>
---
+++ resources/js/components/Battlenet/BattlenetAuthenticateFailed.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="flex items-end my-20 justify-center">
+      <h1>
+        Log in Failed.
+      </h1>
+      <custom-button class="m-2" :href="'/Authenticate/Battlenet'" :text="'Try and log in again'" :alt="'Try and log in again'" :size="'medium'" :color="'blue'"></custom-button>
+    </div>
+  </div>
+</template>
+  
+<script>
+  export default {
+  name: 'ExampleComponent',
+  components: {
+  },
+  props: {
+  },
+  data(){
+    return {
+      selectedRegion: null,
+      filters: {
+        type: Object,
+        required: true
+      },
+    }
+  },
+  created(){
+  },
+  mounted() {
+  },
+  computed: {
+  },
+  watch: {
+  },
+  methods: {
+  }
+  }
+</script>
