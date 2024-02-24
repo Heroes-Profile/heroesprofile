a8a81ad8ef90049d4133c0afe4afdb799ea1e162
Zemill <33404722+Zemill@users.noreply.github.com>
2024-02-24 10:47:23 -0500

    Adds cors allow on base url

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CorsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+      $response = $next($request);
+        
+      $response->headers->set('Access-Control-Allow-Origin', 'https://heroesprofile.com');
+
+      return $response;
+    }
+}
