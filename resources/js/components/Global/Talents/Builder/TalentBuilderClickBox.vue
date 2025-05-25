9dd58802064d6cbb3906341649ff276f3536622c
Tasha Millhouse <tathiebaut@gmail.com>
2025-05-25 09:58:26 -0400

    updated styling of talent builder so it works on mobile - #1151

--- resources/js/components/Global/Talents/Builder/TalentBuilderClickBox.vue
+++ resources/js/components/Global/Talents/Builder/TalentBuilderClickBox.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="border-b text-black flex flex-col justify-stretch  min-h-[250px]">
+  <div class=" text-black flex flex-col justify-stretch min-w-[210px] ">
     <div
-      class="cursor-pointer p-4 transition duration-300 hover:bg-gray-md hover:text-black h-full m-2 min-h-[250px]"
+      class="cursor-pointer p-4 transition duration-300 hover:bg-gray-md hover:text-black h-full m-2 "
       :class="{ 'bg-teal text-white': isClicked }"
     >
       <div class="flex flex-col items-center justify-center text-center">
-       <h3 class="min-h-[80px]"> {{ talent.title }}</h3>
-        <talent-image-wrapper :talent="talent" :size="'medium'" class="mb-10"></talent-image-wrapper>
+       <h3 class="pb-4"> {{ talent.title }}</h3>
+        <talent-image-wrapper :talent="talent" :size="'medium'" class=""></talent-image-wrapper>
         <span v-if="talent.win_rate">{{ talent.win_rate.toFixed(2) }}%</span>
         <span v-if="talent.games_played" class="text-xs">{{ talent.games_played.toLocaleString('en-US') }} games</span>
       </div>
