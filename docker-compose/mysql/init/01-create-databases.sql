c0fd7a38490ed3294afbce90627055f88dcb372b
Zemill <33404722+Zemill@users.noreply.github.com>
2025-11-25 08:15:39 -0500

    Adds new schema to docket setup

--- docker-compose/mysql/init/01-create-databases.sql
+++ docker-compose/mysql/init/01-create-databases.sql
@@ -1,5 +1,6 @@
 -- Create all required databases for Heroes Profile
 CREATE DATABASE IF NOT EXISTS heroesprofile;
+CREATE DATABASE IF NOT EXISTS heroesprofile_globals;
 CREATE DATABASE IF NOT EXISTS heroesprofile_cache;
 CREATE DATABASE IF NOT EXISTS heroesprofile_logs;
 CREATE DATABASE IF NOT EXISTS heroesprofile_ngs;
@@ -10,6 +11,7 @@ CREATE DATABASE IF NOT EXISTS heroesprofile_hi_nc;
 
 -- Grant permissions to the heroesprofile user for all databases
 GRANT ALL PRIVILEGES ON heroesprofile.* TO 'heroesprofile'@'%';
+GRANT ALL PRIVILEGES ON heroesprofile_globals.* TO 'heroesprofile'@'%';
 GRANT ALL PRIVILEGES ON heroesprofile_cache.* TO 'heroesprofile'@'%';
 GRANT ALL PRIVILEGES ON heroesprofile_logs.* TO 'heroesprofile'@'%';
 GRANT ALL PRIVILEGES ON heroesprofile_ngs.* TO 'heroesprofile'@'%';
