baa8d72dde7b9d9a038c93e446a5a91d1b91b5d8
Zemill <33404722+Zemill@users.noreply.github.com>
2025-11-29 11:38:36 -0500

    Removes deprecated package

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from 'tailwindcss'
-import flareSourcemapUploader from '@flareapp/vite-plugin-sourcemap-uploader';
+import flareSourcemapUploader from '@flareapp/vite';
 
 export default defineConfig({
     plugins: [
