6f850344a240e92af67094a1b5cdcdde62c4134d
Zemill <33404722+Zemill@users.noreply.github.com>
2025-11-29 11:55:34 -0500

    Remove test routes

--- routes/web.php
+++ routes/web.php
@@ -61,8 +61,10 @@ Route::fallback(function () {
 // Route::middleware(['logIpAndUserAgent', 'communitySupportRedirect'])->group(function () {
 Route::middleware(['logIpAndUserAgent'])->group(function () {
     Route::get('/', [MainPageController::class, 'show']);
-    Route::get('/test', [MainPageController::class, 'test']);
-    Route::get('/testJS', [MainPageController::class, 'testJS']);
+    
+    //Testing routes
+    //Route::get('/test', [MainPageController::class, 'test']);
+    //Route::get('/testJS', [MainPageController::class, 'testJS']);
 
     Route::redirect('/Search', '/', 301);
 
