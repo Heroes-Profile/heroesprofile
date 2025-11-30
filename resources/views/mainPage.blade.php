cd1c374251f6aa334e1c0c871914a28c4dffbd9e
Zemill <33404722+Zemill@users.noreply.github.com>
2025-11-30 12:26:48 -0500

    Minor descriptor updates

--- resources/views/mainPage.blade.php
+++ resources/views/mainPage.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app', $bladeGlobals)  
 @section('title', 'Home')
 @section('meta_keywords', 'heroes profile, home page, hero statistics, player data')
-@section('meta_description', 'Explore hero statistics, player data, and more on Heroes Profile, your source for in-depth insights into the Heroes of the Storm community.')
+@section('meta_description', 'Explore Heroes of the Storm hero statistics, player data, and more on Heroes Profile, your source for in-depth insights into the Heroes of the Storm community.')
 @section('content')
   <main-page 
     :user="{{ json_encode(Auth::user()) }}"
