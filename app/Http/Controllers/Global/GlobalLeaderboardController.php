e63087a2ee94811c7f61755008988023dba202a2
Zemill <33404722+Zemill@users.noreply.github.com>
2026-09-16 09:47:39 -0400

    Changes leaderboard to have groups to allow more players on less popular leaderboards while preserving top players rank

--- app/Http/Controllers/Global/GlobalLeaderboardController.php
+++ app/Http/Controllers/Global/GlobalLeaderboardController.php
@@ -119,12 +119,14 @@ class GlobalLeaderboardController extends GlobalsInputValidationController
             }
 
             $data = Leaderboard::query()
-                ->select('rank', 'battletag', 'split_battletag', 'blizz_id', 'region', 'win_rate', 'games_played', 'conservative_rating', 'rating', 'normalized_rating', 'most_played_hero', 'level_one', 'level_four', 'level_seven', 'level_ten', 'level_thirteen', 'level_sixteen', 'level_twenty', 'hero_build_games_played')
+                ->select('rank', 'leaderboard_group', 'min_games_required', 'battletag', 'split_battletag', 'blizz_id', 'region', 'win_rate', 'games_played', 'conservative_rating', 'rating', 'normalized_rating', 'most_played_hero', 'level_one', 'level_four', 'level_seven', 'level_ten', 'level_thirteen', 'level_sixteen', 'level_twenty', 'hero_build_games_played')
                 ->filterByGameType($gameType)
                 ->filterBySeason($season)
                 ->filterByType($typeNumber)
                 ->filterByStackSize($groupsize)
                 ->filterByRegion($region)
+                // Rank order keeps each group together and makes the rank shift for hidden accounts below correct
+                ->orderBy('rank')
                 // ->toSql();
                 ->get();
 
